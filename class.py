91378744f8687174dac62b2f457eb986d6706054
marchikk <157586063+marchikk@users.noreply.github.com>
2024-03-20 09:53:04 +0000

    addchanges

--- class.py
+++ class.py
@@ -6,3 +6,8 @@ class DogSpike:
     def walk(self):
         print('Dog walked')
         self.is_walked = True
+my_dog = DogSpike()
+print(my_dog.name)
+print(my_dog.is_walked)
+my_dog.walk()
+print(my_dog.is_walked)
