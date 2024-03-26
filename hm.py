798320ccee64b694ad24aa492a59ef43660a9450
marchikk <157586063+marchikk@users.noreply.github.com>
2024-03-26 17:49:39 +0000

    addchanmges

--- hm.py
+++ hm.py
@@ -1,17 +1,12 @@
 class DogRex:
     name = 'Rex'
     def __init__(self):
-        self.is_walked = False
-    def walk(self):
-        print('Dog walked')
-        self.is_walked = True
-
-
+        self.is_hungry = False
+    def hungry(self):
+        print('Dog is hungry')
+        self.is_hungry = True
 my_dog = DogRex()
-print('my dog name:', my_dog.name) # Rex
-print('my dog voice:', my_dog.voice) # bow-wow
-print('my dog is hungry:', my_dog.hungry) # True
-my_dog.feed()
-print('my dog is hungry:', my_dog.hungry) # False
-my_dog.walk()
-print('my dog is hungry:', my_dog.hungry) # True
+print(my_dog.name)
+print(my_dog.is_hungry)
+my_dog.hungry()
+print(my_dog.is_hungry)
