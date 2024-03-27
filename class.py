a9570b0b9624c49728a75666fdd026d952773f44
marchikk <157586063+marchikk@users.noreply.github.com>
2024-03-27 09:53:40 +0000

    addchanges

--- class.py
+++ class.py
@@ -1,13 +1,45 @@
 
-class DogSpike:
-    name = 'Spike'
-    def __init__(self):
+class Dog:
+    name = 'unknown'
+    def __init__(self, breed , speed):
         self.is_walked = False
+        self.speed = speed
+        self.breed = breed
+        self.tricks = []
+
     def walk(self):
-        print('Dog walked')
+        print(f'dog {self.name} walks')
         self.is_walked = True
-my_dog = DogSpike()
-print(my_dog.name)
-print(my_dog.is_walked)
-my_dog.walk()
-print(my_dog.is_walked)
+
+    def learn_tricks(trick):
+        self.tricks += [trick] 
+
+    def do_tricks(self):
+        for trick in self.tricks:
+            print(f'dog{self.name} of breed {self.breed} performs trick{trick}')
+        if self.tricks ==[]:
+            print(f'')
+    
+
+class DogSpike(Dog):
+    name = 'Spike'
+
+    def run(self):
+        print(f'dog{self.name} is running {self.speed} km/h and is hungry')
+
+class DogMike(Dog):
+    name = 'Mike'
+
+    def run(self):
+        print(f'dog{self.name} is running {self.speed} km/h and is thirsty')
+
+my_dog = DogSpike('bulldog' , 12)
+friends_dog = DogMike('pitbull', 20)
+my_dog.learn_trick('sit down')
+print(f'my dog name is {my_dog.name}')
+print(f'my dog speed is {my_dog.speed} km/h')
+print(f'my friend\'s dog name is {friends_dog.name}')
+print(f'my friend\'s dog speed is {friends_dog.speed}')
+my_dog.run()
+friends_dog.run()
+
