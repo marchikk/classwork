7cb4265c4f6c8aa0beece36e4fdd0988e657081a
marchikk <157586063+marchikk@users.noreply.github.com>
2024-04-03 08:52:15 +0000

    aaa

---
+++ hm10.py
@@ -0,0 +1,10 @@
+
+class DogRex:
+    name = 'Rex'
+    my_dog.voice = 
+    def __init__(self):
+        self.is_hungry = True
+    def hungry(self):
+        print('Dog is hungry')
+
+        
---
+++ hm5.py
@@ -0,0 +1,17 @@
+print("Hello , how are you?")
+user_feeling = input()
+
+print(f"Great , that you are {user_feeling}! What is your name?")
+user_name = input()
+
+print(f"Hello , {user_name}! How old are you?")
+user_age =int(input())
+
+current_year = 2024
+birth_year = current_year - user_age
+
+
+print(f"I see , you were born in year {birth_year}? Let me know if its True or False?")
+user_response = input()
+
+
